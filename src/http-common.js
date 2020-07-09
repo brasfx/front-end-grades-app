0d2710c2fb2bf3b784527f2544a5103941519274
davi.ribeiro <foxpc18@gmail.com>
2020-07-08 22:47:26 -0300

    Modificando cors

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: `https://back-end-grades-api.herokuapp.com`,
+  baseURL: ` ${process.env.REACT_APP_BASE_URL}`,
   headers: {
     'Content-type': 'application/json',
   },
