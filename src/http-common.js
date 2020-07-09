456f90b91392160590197e6f74ef6c3926ca9a72
davi.ribeiro <foxpc18@gmail.com>
2020-07-08 22:00:31 -0300

    Atualizando URL Heroku

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //Define a URL base da origem para consumo do servico
 //Adicionando variavel ambiente para conectar ao Heroku
 export default axios.create({
-  baseURL: ` ${process.env.REACT_APP_BASE_URL}`,
+  baseURL: ` ${process.env.REACT_APP_BASE_URL_HEROKU}`,
   headers: {
     'Content-type': 'application/json',
   },
