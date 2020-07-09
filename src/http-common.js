9fc35d0e5bf524441547aba7b6a8bcf37447124a
davi.ribeiro <foxpc18@gmail.com>
2020-07-08 22:14:45 -0300

    Atualizando rota heroku

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
-//Adicionando variavel ambiente para conectar ao Heroku
 export default axios.create({
-  baseURL: ` ${process.env.REACT_APP_BASE_URL_HEROKU}`,
+  baseURL: ` ${process.env.REACT_APP_BASE_URL}`,
   headers: {
     'Content-type': 'application/json',
   },
 });
+
+console.log(process.env.REACT_APP_BASE_URL);
